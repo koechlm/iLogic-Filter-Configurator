25959286622923cfbd77539dd8fff986c4582384
koechlm <markus.koechl@autodesk.com>
2019-10-01 14:25:39 +0200

    Add project files.

---
+++ iLogic Filter Configurator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLogic_Filter_Configurator
+{
+    public class Class1
+    {
+    }
+}
